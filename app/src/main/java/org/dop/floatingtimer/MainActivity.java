839f46352b8d3e7c98a66cc33968ab349e73fbd7
si tran <19522128@gm.uit.edu.vn>
2023-11-15 18:00:59 +0700

    create main interface

--- app/src/main/java/org/dop/floatingtimer/MainActivity.java
+++ app/src/main/java/org/dop/floatingtimer/MainActivity.java
@@ -1,14 +1,29 @@
 package org.dop.floatingtimer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.opt_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
